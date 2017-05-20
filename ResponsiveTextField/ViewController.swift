952522360fdbb96abb674a6e75c1f9d870143cfe
Afriyandi Setiawan <afreeyandi@gmail.com>
2017-05-21 00:51:26 +0700

    Add view for storyboard

---
+++ ResponsiveTextField/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ResponsiveTextField
+//
+//  Created by Afriyandi Setiawan on 5/20/17.
+//  Copyright © 2017 Afriyandi Setiawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
